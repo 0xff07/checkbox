e32cec69b2dbb3fc2fbb165b67a926a7bd23bb78
Alex Tu <alex.tu@canonical.com>
2020-09-29 12:38:25 +0800

    force manager review MP for oem-fix packages
    fix shellcheck error as well

--- bin/screen-pkg-not-public.sh
+++ bin/screen-pkg-not-public.sh
@@ -8,7 +8,7 @@ allowlst_folder=""
 STATUS="pass"
 clean() {
    rm -rf "$allowlst_folder"
-   [ -z "$1" ] || exit $1
+   [ -z "$1" ] || exit "$1"
    [ "$STATUS" != "pass" ] && exit 1
    exit 0
 }
@@ -22,12 +22,12 @@ prepare() {
            platform="$(ubuntu-report show | grep DCD | awk -F'+' '{print $2}')"
            ;;
        "sutton")
-           platform_meta="$(dpkg-query -W -f='${Package}\n'  "oem-"$oem".*-meta")"
+           platform_meta="$(dpkg-query -W -f='${Package}\n'  "oem-""$oem"".*-meta")"
            oem="$(echo "$platform_meta" | cut -d'-' -f2 )"
            platform="$(echo "$platform_meta" | cut -d'-' -f3 )"
            ;;
        "stella")
-           platform_meta="$(dpkg-query -W -f='${Package}\n'  "oem-"$oem".*-meta")"
+           platform_meta="$(dpkg-query -W -f='${Package}\n'  "oem-""$oem"".*-meta")"
            oem="$(echo "$platform_meta" | cut -d'-' -f2 )"
            platform="$(echo "$platform_meta" | cut -d'-' -f3 )"
            ;;
@@ -39,23 +39,39 @@ prepare() {
     (sudo apt-get update > /dev/null || (>&2 echo "[ERROR]apt-get update failed, please check it." | exit 1)) && sudo apt-get install -y git > /dev/null
     echo "[INFO] getting allowlist from $allowlist_git."
     [ -n "$allowlist_git" ] &&\
-    (git clone --depth=1 "$allowlist_git" || (>&2 echo "[ERROR]git clone "$allowlist_git" filed, please check it." | exit 1)) &&\
+    (git clone --depth=1 "$allowlist_git" || (>&2 echo "[ERROR]git clone ""$allowlist_git"" filed, please check it." | exit 1)) &&\
     allowlst_folder="$PWD"/"$(basename "$allowlist_git")"
     echo "[INFO] git hash of current allowlist: $(git -C "$allowlst_folder" rev-parse --short HEAD)"
 }
+pkg_need_allowing() {
+    [ -z "$1" ] && >&2 echo "[ERROR][CODE]got an empty pkg in ${FUNCNAME[1]}" && clean 1
+    >&2 echo "[ERROR] Please send a MP to $allowlist_git for manager review $1" && STATUS="failed"
+    clean
+}
 pkg_need_update() {
     [ -z "$1" ] && >&2 echo "[ERROR][CODE]got an empty pkg in ${FUNCNAME[1]}" && clean 1
-    >&2 echo "[ERROR] find a update-able pkg: $1 $2" && STATUS="failed"
+    >&2 echo "[ERROR] find a update-able pkg: $1 $2" && pkg_need_allowing "$1"
+}
+if_allowing() {
+    local allowed="NO"
+    [ -z "$1" ] && >&2 echo "[ERROR][CODE]got an empty pkg in ${FUNCNAME[1]}" && clean 1
+
+    # check if the pkg on allow list.
+    for F in "$allowlst_folder"/testtools "$allowlst_folder"/common "$allowlst_folder"/"$oem"/common "$allowlst_folder"/"$oem"/"$platform"; do
+       [ -f "$F" ] && while IFS= read -r green_light; do
+       [ "$1" == "$green_light" ] && echo "[INFO] manager gave a greenlight for :" "$1" "$2" && allowed="YES"
+       done < "$F"
+    done
+    if [ "$allowed" == "NO" ]; then
+	   return 1
+    else
+	   return 0
+    fi
 }
 pkg_not_public() {
     [ -z "$1" ] && >&2 echo "[ERROR][CODE]got an empty pkg in" "${FUNCNAME[1]}" && clean 1
     # check if the pkg on allow list.
-    for F in "$allowlst_folder"/testtools "$allowlst_folder"/common "$allowlst_folder"/"$oem"/common "$allowlst_folder"/"$oem"/"$platform"; do
-       [ -f "$F" ] && while IFS= read -r green_light; do
-           [ "$1" == "$green_light" ] && echo "[INFO] manager gave a greenlight for :" "$1" "$2" && return 0
-       done < "$F"
-    done
-    >&2 echo "[ERROR] find a packge not on public archive:" "$1" "$2"&& STATUS="failed"
+    if_allowing "$1" || (>&2 echo "[ERROR] find a packge not on public archive:" "$1" "$2"&& pkg_need_allowing "$1")
 }
 prepare
 echo "[INFO] staring screen all installed packages."
@@ -65,6 +81,10 @@ while IFS= read -r pkgname; do
    pkgver="$(dpkg-query -W -f='${Version}' "$pkgname")"
    pub_madison="$(apt-cache madison  "$pkgname")"
    can_pkgver="$(apt-cache policy  "$pkgname" | grep Candidate | awk '{print $2}')"
+   if [ -z "${pkgname##oem-fix*}" ]; then
+        if_allowing "$pkgname" || pkg_need_allowing "$pkgname"
+   fi
+
    if [ -z "$pub_madison" ]; then
         pkg_not_public "$pkgname" "$pkgver"
    elif [ -n "${pub_madison##*$can_pkgver*}" ]; then
