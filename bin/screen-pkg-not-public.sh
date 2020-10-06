60f8d2031fb99873fe6d04c4b1dae42537b76cca
Alex Tu <alex.tu@canonical.com>
2020-10-06 18:03:03 +0800

    rearranged for test cases

--- bin/screen-pkg-not-public.sh
+++ bin/screen-pkg-not-public.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 set -e
+pkg_pass="0"
+pkg_failed="1"
 allowlist_git="https://git.launchpad.net/~oem-solutions-engineers/pc-enablement/+git/oem-gap-allow-list"
+pf_meta_pkg=""
+pf_factory_meta_pkg=""
 oem=""
 platform=""
 allowlst_folder=""
-STATUS="pass"
+JOB_STATUS="pass"
 clean() {
    rm -rf "$allowlst_folder"
    [ -z "$1" ] || exit "$1"
-   [ "$STATUS" != "pass" ] && exit 1
+   [ "$JOB_STATUS" != "pass" ] && exit 1
    exit 0
 }
 prepare() {
@@ -38,15 +42,18 @@ prepare() {
     esac
     [ -z "$platform" ] && (>&2 echo "[ERROR][CODE]got an empty platform name for $oem in ${FUNCNAME[1]}" && clean 1)
     (sudo apt-get update > /dev/null || (>&2 echo "[ERROR]apt-get update failed, please check it." | exit 1)) && sudo apt-get install -y git > /dev/null
+    pf_meta_pkg="$(dpkg -S /etc/apt/sources.list.d/oem-"${oem}"-*-meta.list | awk '{print $1}' | sed 's/://Ig')" || pf_meta_pkg=""
+    pf_factory_meta_pkg="${pf_meta_pkg/oem-${oem}-/oem-${oem}-factory-}" || pf_factory_meta_pkg=""
     echo "[INFO] getting allowlist from $allowlist_git."
     [ -n "$allowlist_git" ] &&\
-    (git clone --depth=1 "$allowlist_git" || (>&2 echo "[ERROR]git clone ""$allowlist_git"" filed, please check it." | exit 1)) &&\
-    allowlst_folder="$PWD"/"$(basename "$allowlist_git")"
+    allowlst_folder="$PWD"/"$(basename "$allowlist_git")" &&\
+    rm -rf $allowlst_folder &&\
+    (git clone --depth=1 "$allowlist_git" || (>&2 echo "[ERROR]git clone ""$allowlist_git"" failed, please check it." | exit 1))
     echo "[INFO] git hash of current allowlist: $(git -C "$allowlst_folder" rev-parse --short HEAD)"
 }
 pkg_need_allowing() {
     [ -z "$1" ] && >&2 echo "[ERROR][CODE]got an empty pkg in ${FUNCNAME[1]}" && clean 1
-    >&2 echo "[ERROR] Please send a MP to $allowlist_git for manager review $1" && STATUS="failed"
+    >&2 echo "[ERROR] Please send a MP to $allowlist_git for manager review $1" && JOB_STATUS="failed"
 }
 pkg_need_update() {
     [ -z "$1" ] && >&2 echo "[ERROR][CODE]got an empty pkg in ${FUNCNAME[1]}" && clean 1
@@ -73,76 +80,86 @@ if_allowing() {
 pkg_not_public() {
     [ -z "$1" ] && >&2 echo "[ERROR][CODE]got an empty pkg in" "${FUNCNAME[1]}" && clean 1
     # check if the pkg on allow list.
-    if_allowing "$1" || (>&2 echo "[ERROR] find a packge not on public archive:" "$1" "$2" && pkg_need_allowing "$1")
+    if_allowing "$1" || (>&2 echo "[ERROR] find a packge not on public archive:" "$1" "$2" && pkg_need_allowing "$1" && return $pkg_failed)
 }
-prepare
-echo "[INFO] staring screen all installed packages."
-
-pf_meta_pkg="$(dpkg -S /etc/apt/sources.list.d/oem-"${oem}"-*-meta.list | awk '{print $1}' | sed 's/://Ig')"
-pf_factory_meta_pkg="${pf_meta_pkg/oem-${oem}-/oem-${oem}-factory-}"
-
-while IFS= read -r line; do
-    progress=">""$progress"
-    [ "${#progress}" == "70" ] && echo "$progress" && progress=""
-
-    pkg_name="$(echo "${line}" | awk '{print $2}')"
-    pkg_ver="$(echo "${line}" | awk '{print $3}')"
-    pkg_curr_madison="$(apt-cache madison "${pkg_name}" | grep "$pkg_ver" || true)"
-
-    # FIXME: I don't think the upgradable is need to in "id: miscellanea/screen-pkg-not-public"
-    # Should have the other something like "id: miscellanea/check-oem-pkg-updatable"
-    # Remove this section can speed up this test scope
-    # Alarm if package is old
-    # TODO: detect somerville only?
-    if [ -z "${pkg_ver##*oem*}" ] || [ -z "${pkg_ver##*somerville*}" ]; then
-        if if_allowing "$pkg_name"; then
-            continue
-        else
-            can_pkg_ver="$(apt-cache policy "$pkg_name" | grep Candidate | awk '{print $2}')"
-            if dpkg --compare-versions "$can_pkg_ver" "gt" "$pkg_ver"; then
-                pkg_need_update "$pkg_name" "$pkg_ver"
+screen_pkg() {
+    [ -n "$1" ] || (>&2 echo "[ERROR][CODE]got an empty input in" "${FUNCNAME[1]}" && clean 1)
+    line="$1"
+        pkg_name="$(echo "${line}" | awk '{print $2}')"
+        pkg_ver="$(echo "${line}" | awk '{print $3}')"
+        pkg_curr_madison="$(apt-cache madison "${pkg_name}" | grep "$pkg_ver" || true)"
+        # FIXME: I don't think the upgradable is need to in "id: miscellanea/screen-pkg-not-public"
+        # Should have the other something like "id: miscellanea/check-oem-pkg-updatable"
+        # Remove this section can speed up this test scope
+        # Alarm if package is old
+        # TODO: detect somerville only?
+        if [ -z "${pkg_ver##*oem*}" ] || [ -z "${pkg_ver##*somerville*}" ]; then
+            if if_allowing "$pkg_name"; then
+                return $pkg_pass
+            else
+                can_pkg_ver="$(apt-cache policy "$pkg_name" | grep Candidate | awk '{print $2}')"
+                if dpkg --compare-versions "$can_pkg_ver" "gt" "$pkg_ver"; then
+                    pkg_need_update "$pkg_name" "$pkg_ver"
+                fi
             fi
         fi
-    fi
-
-    # If empty then meaning on one know where is this package come from
-    # (e.g. a package only in CESG)
-    if [ -z "${pkg_curr_madison}" ]; then
-        can_pkg_ver="$(apt-cache policy "$pkg_name" | grep Candidate | awk '{print $2}')"
-        pkg_can_madison="$(apt-cache madison "${pkg_name}" | grep "$can_pkg_ver" || true)"
-        # If the candidate version is from ubuntu-archive then it'll be
-        #  covered by SRU process.
-        if [ -n "${pkg_can_madison}" ] &&
-            { [ -z "${pkg_can_madison##*security.ubuntu.com/ubuntu*}" ] ||
-            [ -z "${pkg_can_madison##*archive.ubuntu.com/ubuntu*}" ]; }; then
-            continue
+    
+        # If empty then meaning on one know where is this package come from
+        # (e.g. a package only in CESG)
+        if [ -z "${pkg_curr_madison}" ]; then
+            can_pkg_ver="$(apt-cache policy "$pkg_name" | grep Candidate | awk '{print $2}')"
+            pkg_can_madison="$(apt-cache madison "${pkg_name}" | grep "$can_pkg_ver" || true)"
+            # If the candidate version is from ubuntu-archive then it'll be
+            #  covered by SRU process.
+            if [ -n "${pkg_can_madison}" ] &&
+                { [ -z "${pkg_can_madison##*security.ubuntu.com/ubuntu*}" ] ||
+                [ -z "${pkg_can_madison##*archive.ubuntu.com/ubuntu*}" ]; }; then
+                return $pkg_pass
+            fi
+            pkg_not_public "$pkg_name" "$pkg_ver" || return $pkg_failed
         fi
-        pkg_not_public "$pkg_name" "$pkg_ver"
-        continue
-    fi
-
-    # If the installed package is from ubuntu-archive then we're good
-    # (no matter which version is candidate because we expected the
-    #  all source list are under control at lease before GM)
-    if [ -z "${pkg_curr_madison##*security.ubuntu.com/ubuntu*}" ] ||
-        [ -z "${pkg_curr_madison##*archive.ubuntu.com/ubuntu*}" ]; then
-        continue
-    fi
-
-    # If the installed package is from canonical-archive then we need to
-    #  make sure the packages were review.
-    if [ -z "${pkg_curr_madison##*archive.canonical.com*}" ]; then
-        # If the package is platform meta package then it should be control by meta generator
-        if [ "$pkg_name" == "$pf_meta_pkg" ] || [ "$pkg_name" == "$pf_factory_meta_pkg" ]; then
-            continue
+    
+        # If the installed package is from ubuntu-archive then we're good
+        # (no matter which version is candidate because we expected the
+        #  all source list are under control at lease before GM)
+        if [ -z "${pkg_curr_madison##*security.ubuntu.com/ubuntu*}" ] ||
+            [ -z "${pkg_curr_madison##*archive.ubuntu.com/ubuntu*}" ]; then
+            >&2 echo "echo [INFO] perfect passs"
+            #return $pkg_pass
+            return 0
         fi
-        # Otherwise, need to review
-        pkg_not_public "$pkg_name" "$pkg_ver"
-        continue
-    fi
+    
+        # If the installed package is from canonical-archive then we need to
+        #  make sure the packages were review.
+        if [ -z "${pkg_curr_madison##*archive.canonical.com*}" ]; then
+            # If the package is platform meta package then it should be control by meta generator
+            if [ "$pkg_name" == "$pf_meta_pkg" ] || [ "$pkg_name" == "$pf_factory_meta_pkg" ]; then
+                return $pkg_pass
+            fi
+            # Otherwise, need to review
+            pkg_not_public "$pkg_name" "$pkg_ver" || return $pkg_failed
+        fi
+        # For unkown source of package (e.g. from a ppa), then review
+        pkg_not_public "$pkg_name" "$pkg_ver" || return $pkg_failed
+        #if [ "$JOB_STATUS" = "pass" ]; then
+        #    >&2 echo "function failed."
+        #    return $pkg_failed
+        #else
+        #    >&2 echo "function passed."
+        #fi
+}
 
-    # For unkown source of package (e.g. from a ppa), then review
-    pkg_not_public "$pkg_name" "$pkg_ver"
-
-done < <(dpkg -l | grep 'ii')
-clean
+run_main() {
+    prepare
+    >&2 echo "[INFO] staring screen all installed packages."
+    while IFS= read -r line; do
+        progress=">""$progress"
+        [ "${#progress}" == "70" ] && echo "$progress" && progress=""
+        screen_pkg $line
+    done < <(dpkg -l | grep 'ii')
+    clean
+}
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  >&2 echo "entering run_main"
+  run_main
+fi
---
+++ test/test-screen-pkg-not-public.bat
@@ -0,0 +1,140 @@
+#!/usr/bin/env bash
+
+BIN_FOLDER="../bin"
+
+function if_allowing() {
+    return 1
+}
+
+function setup() {
+    source "$BIN_FOLDER"/screen-pkg-not-public.sh
+    oem="somerville"
+    platform="fossa-melisa"
+}
+
+function teardown() {
+    >&2 echo "JOB_STATUS="$JOB_STATUS
+}
+
+function apt-cache() {
+   case "$1" in
+    "madison")
+    echo "$aptcache_medison_string"
+    ;;
+    "policy")
+    echo "$apt_cache_policy_string"
+    ;;
+    *)
+    return 1;
+    ;;
+   esac
+}
+
+@test "allow pkgs came from ubuntu archive" {
+    set -e
+    aptcache_medison_string="
+    ubuntu-desktop |    1.450.2 | http://archive.ubuntu.com/ubuntu focal-updates/main amd64 Packages
+    ubuntu-desktop |      1.450 | http://archive.ubuntu.com/ubuntu focal/main amd64 Packages
+    "
+    apt_cache_policy_string="\
+    ubuntu-desktop:\
+      Installed: 1.450.2\
+      Candidate: 1.450.2\
+      Version table:\
+     *** 1.450.2 500\
+            500 http://archive.ubuntu.com/ubuntu focal-updates/main amd64 Packages\
+            100 /var/lib/dpkg/status\
+         1.450 500\
+            500 http://archive.ubuntu.com/ubuntu focal/main amd64 Packages\
+    "
+    export -f apt-cache
+    # we expect it passed.
+    screen_pkg "ii  ubuntu-desktop                                1.450.2                                     amd64        The Ubuntu desktop system"
+    [ "$?" == "0" ]
+    unset -f apt-cache
+}
+
+@test "screen out pkgs we hacked" {
+    set -e
+    dpkg_list_string="ii  ubiquity                                      20.04.15.2somerville2                       amd64        Ubuntu live CD installer"
+    aptcache_medison_string="
+    ubiquity | 20.04.15.2 | http://archive.ubuntu.com/ubuntu focal-updates/main amd64 Packages
+    ubiquity |   20.04.15 | http://archive.ubuntu.com/ubuntu focal/main amd64 Packages
+    "
+    apt_cache_policy_string="
+    ubiquity:
+      Installed: 20.04.15.2somerville2
+      Candidate: 20.04.15.2somerville2
+      Version table:
+     *** 20.04.15.2somerville2 100
+            100 /var/lib/dpkg/status
+         20.04.15.2 500
+            500 http://archive.ubuntu.com/ubuntu focal-updates/main amd64 Packages
+         20.04.15 500
+            500 http://archive.ubuntu.com/ubuntu focal/main amd64 Packages
+    "
+    export -f apt-cache
+    # we expect it failed.
+    ! screen_pkg "$dpkg_list_string"
+    unset -f apt-cache
+}
+
+@test "screen out pkgs only in unexpected archive." {
+    set -e
+    dpkg_list_string="ii  fwts                                          20.09.00-0ubuntu1~f                         amd64        FirmWare Test Suite"
+    aptcache_medison_string="
+    fwts | 20.09.00-0ubuntu1~f | http://ppa.launchpad.net/checkbox-dev/ppa/ubuntu focal/main amd64 Packages
+    fwts | 20.03.00-0ubuntu1 | http://archive.ubuntu.com/ubuntu focal/universe amd64 Packages
+    "
+    apt_cache_policy_string="
+    fwts:
+      Installed: 20.09.00-0ubuntu1~f
+      Candidate: 20.09.00-0ubuntu1~f
+      Version table:
+     *** 20.09.00-0ubuntu1~f 500
+            500 http://ppa.launchpad.net/checkbox-dev/ppa/ubuntu focal/main amd64 Packages
+            100 /var/lib/dpkg/status
+         20.03.00-0ubuntu1 500
+            500 http://archive.ubuntu.com/ubuntu focal/universe amd64 Packages
+    "
+
+    export -f apt-cache
+    # we expect it failed.
+    ! screen_pkg "$dpkg_list_string"
+    unset -f apt-cache
+}
+
+@test "screen out pkgs not in any archive." {
+    set -e
+    # we expect it failed.
+    ! screen_pkg "ii  a-nonexisted-pkg                                1.187.3                                     all          Firmware for Linux kernel drivers"
+    echo in bat: JOB_STATUS=$JOB_STATUS
+}
+
+@test "screen out pkgs only on oem archive." {
+    function apt-cache() {
+       case "$1" in
+        "madison")
+        echo "pkg-only-on-oem-archive | 20.04ubuntu7 | http://dell.archive.canonical.com focal/somerville-melisa amd64 Packages"
+        ;;
+        "policy")
+        echo "
+        pkg-only-on-oem-archive:
+         Installed: 20.04ubuntu7
+         Candidate: 20.04ubuntu7
+         Version table:
+        *** 20.04ubuntu7 500
+        500 http://dell.archive.canonical.com focal/somerville-melisa amd64 Packages
+        500 http://dell.archive.canonical.com focal/somerville-melisa i386 Packages
+        "
+        ;;
+        *)
+        return 1;
+        ;;
+       esac
+    }
+    export -f apt-cache
+    # we expect it failed.
+    ! screen_pkg "ii  pkg-only-on-oemarchive                                1.187.3                                     all          Firmware for Linux kernel drivers"
+    unset -f apt-cache
+}
