7b24a2b24f0b531418328d2d6d355bbeed2c7d78
Alex Tu <alex.tu@canonical.com>
2020-12-25 14:35:08 +0800

    polish by reviewer's comment

--- bin/cvescan.sh
+++ bin/cvescan.sh
@@ -13,11 +13,11 @@ exit 0
 prepare() {
 # https://github.com/canonical/sec-cvescan
     for pkg in python3-apt python3-pip git jq; do
-        dpkg-query -W -f='${Status}' "$pkg" || sudo apt install "$pkg"
+        [ "$(dpkg-query -W -f='${Status}' "$pkg")" = "install ok installed" ] || (sudo apt-get update && DEBIAN_FRONTEND=noninteractive sudo apt install -y "$pkg")
     done
     git clone https://github.com/canonical/sec-cvescan
     export PATH=$HOME/.local/bin/:$PATH
-    pip3 install --user sec-cvescan/
+    pip3 install --user sec-cvescan/ > /dev/null
     rm -rf sec-cvescan
 }
 main() {
@@ -40,7 +40,7 @@ main() {
 
     prepare
     get_cvescan_json  "$OUT/cvescan.json"
-    pase_cvescan_json "$OUT/cvescan.json"
+    parse_cvescan_json "$OUT/cvescan.json"
 }
 
 get_cvescan_json() {
@@ -49,17 +49,17 @@ get_cvescan_json() {
     cvescan --json > "$1"
 }
 
-pase_cvescan_json() {
+parse_cvescan_json() {
     [ -n "$1" ] || exit 1
     command -v jq || exit 1
-    if [ "$(jq ".summary.num_cves" < "$1")" != "0" ]; then
-        echo "You have some packages need update for CVE."
-        echo "Please check $1 for detail"
+    if [ "$(jq -r '.summary.num_cves' < "$1")" != "0" ]; then
+        echo "You have some packages that need an update for CVE."
+        echo "Please check $1 for details"
         exit 1
     fi
 }
 
-if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
     main "$@"
 fi
 
