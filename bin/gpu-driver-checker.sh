cdf6eb2c7a0da3d5fc8622529524b74eb7e838e3
Jeremy Szu <jeremy.szu@canonical.com>
2022-02-18 21:12:21 +0800

    Improve gpu-driver-checker.sh
    1. Stock ubuntu user might not get nvidia-driver-470 when fresh
       install since Jammy but linux-module-nvidia-*.
    2. In some ubuntu user cases, user can install both dkms and pre-signed
       nvidia and the reason is making sense if user is a developer who
       needs to have dkms to cover customized kernel cases.
    3. Add check-gpu-driver in other test plan.

--- bin/gpu-driver-checker.sh
+++ bin/gpu-driver-checker.sh
@@ -19,37 +19,29 @@ for gpu in $(lspci -n -d ::0x0300| awk '{print $1}') \
     fi
 done
 
-# Nvidia driver check
-for pkg in $(dpkg-query -W -f='${Package}\n' "nvidia-driver-???"); do
-    if [ "$(dpkg-query -W -f='${Status}' "$pkg")" != "install ok installed" ]; then
-        continue
-    fi
-    echo "${pkg} is installed:"
-    SUPPORT=$(apt show "$pkg" 2>/dev/null| grep "^Support:"| awk '{print $2}')
-    if [ "$SUPPORT" != "LTSB" ]; then
-        echo "E: Your $pkg is not LTS version, please check."
-        apt-cache madison "$pkg"
-        result=255
-    else
-        echo "Your $pkg is LTS version."
-    fi
 
-    VERSION=${pkg##*-}
-    if [ "$(dpkg-query -W -f='${Status}\n' "linux-modules-nvidia-${VERSION}-$(uname -r)")" \
-            != "install ok installed" ]; then
-        echo "E: Your $pkg is not pre-signed driver."
-        echo "E: Expecting linux-modules-nvidia-${VERSION}-$(uname -r)."
-        result=255
-    else
-        echo "Your $pkg is pre-signed linux-modules-nvidia-${VERSION}-$(uname -r)."
-    fi
-
-    if [ "$(dpkg-query -W -f='${Status}\n' "nvidia-dkms-${VERSION}")" \
-            = "install ok installed" ]; then
-        echo "E: Your $pkg shouldn't use dkms version but pre-signed."
-        echo "E: Expecting linux-modules-nvidia-${VERSION}-$(uname -r)."
-        result=255
-    fi
-done
+# Check nvidia driver
+nvidia_version=$(modinfo nvidia| grep "^version"| awk '{print $2}')
+if [ -n "$nvidia_version" ]; then
+	nvidia_pkg_prefix="nvidia-driver-"
+	signed_nvidia_prefix="linux-modules-nvidia"
+	echo "Nvidia version is ${nvidia_version}."
+	if ! modinfo nvidia| grep -q "^signer:.*Canonical Ltd. Kernel Module Signing"; then
+	    echo "E: Your nvidia driver is not signed by Canonical."
+	    echo "E: Expecting ${signed_nvidia_prefix}-${nvidia_version%.*}-$(uname -r)."
+	    result=255
+	else
+	    echo "Nvidia driver is signed."
+	fi
+	pkg="${nvidia_pkg_prefix}${nvidia_version%.*}"
+	support=$(apt show "${pkg}" 2>/dev/null| grep "^Support:"| awk '{print $2}')
+	if [ "$support" != "LTSB" ]; then
+	    echo "E: This nvidia is not LTS version, please check."
+	    apt-cache madison "$pkg"
+	    result=255
+	else
+	    echo "Nvidia driver is LTS version."
+	fi
+fi
 
 exit $result
